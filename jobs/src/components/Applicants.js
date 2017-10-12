f6be49a6cc1974a68a1b6fa8e225d2cfc75dec15
Josh <joshpitzalis@gmail.com>
2017-10-12 10:29:06 +0530

    phoen number added

--- jobs/src/components/Applicants.js
+++ jobs/src/components/Applicants.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { database, auth } from '../firebase';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from 'react'
+import { database, auth } from '../firebase'
+import { Redirect } from 'react-router-dom'
 import {
   Page,
   Layout,
@@ -8,35 +8,35 @@ import {
   Checkbox,
   DisplayText,
   Button
-} from '@shopify/polaris';
+} from '@shopify/polaris'
 
 export default class Applicants extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       name: null,
       applicants: null,
       to: null
-    };
+    }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     database.ref(`jobs/${this.props.match.params.jobId}`).on('value', snap => {
       if (snap.val().applicants) {
         this.setState({
           applicants: Object.values(snap.val().applicants),
           name: snap.val().name || null
-        });
+        })
       } else {
         this.setState({
           name: snap.val().name
-        });
+        })
       }
-    });
+    })
   }
-  render() {
+  render () {
     if (this.state.to) {
-      return <Redirect to={this.state.to} />;
+      return <Redirect to={this.state.to} />
     }
     return (
       <Page
@@ -57,20 +57,20 @@ export default class Applicants extends Component {
       >
         <Layout>
           <Layout.Section>
-            <div className="w-100 flex">
-              <div className="w-33 tl ">
-                <DisplayText size="small">Name</DisplayText>
+            <div className='w-100 flex'>
+              <div className='w-33 tl'>
+                <DisplayText size='small'>Name</DisplayText>
               </div>
-              <div className="w-33 tl">
-                <DisplayText size="small">Contact</DisplayText>
+              <div className='w-33 tl pl2'>
+                <DisplayText size='small '>Contact</DisplayText>
               </div>
-              <div className="w-33 tl">
-                <DisplayText size="small">Submissions</DisplayText>
+              <div className='w-33 tl pl2'>
+                <DisplayText size='small '>Submissions</DisplayText>
               </div>
             </div>
-            <ul className="list pl0 mt0 center">
-              {this.state.applicants ? (
-                this.state.applicants.map((applicant, index) => (
+            <ul className='list pl0 mt0 center'>
+              {this.state.applicants
+                ? this.state.applicants.map((applicant, index) => (
                   <Applicant
                     name={applicant.name}
                     email={applicant.email}
@@ -78,60 +78,65 @@ export default class Applicants extends Component {
                     application={applicant.WTCSApplication}
                     resume={applicant.resume}
                     address={applicant.address}
+                    phone={applicant.phone}
                     key={index}
-                  />
-                ))
-              ) : (
-                <div className="pt5 tc">
-                  <DisplayText size="large">No Applicants yet</DisplayText>
-                </div>
-              )}
+                    />
+                  ))
+                : <div className='pt5 tc'>
+                  <DisplayText size='large'>No Applicants yet</DisplayText>
+                </div>}
             </ul>
           </Layout.Section>
         </Layout>
       </Page>
-    );
+    )
   }
 }
 
-const Applicant = ({ name, email, cv, application, resume, address }) => (
-  <li className="flex wrap mxb cxs lh-copy pa3 ph0-l bb b--black-10">
-    <div className="w-33-l w-33-m w-100">
+const Applicant = ({
+  name,
+  email,
+  cv,
+  application,
+  resume,
+  address,
+  phone
+}) => (
+  <li className='flex wrap mxb cxs lh-copy pa3 ph0-l bb b--black-10'>
+    <div className='w-33-l w-33-m w-100'>
       <DisplayText>{name}</DisplayText>
     </div>
-    <div className="w-33-l w-33-m w-100 overflow-hidden">
-      <DisplayText>{email}</DisplayText>
-      <DisplayText size="small">{address}</DisplayText>
+    <div className='w-33-l w-33-m w-100 overflow-hidden'>
+      <DisplayText size='small'>{phone}</DisplayText>
+      <DisplayText size='small'>{email}</DisplayText>
+      <DisplayText size='small'>{address}</DisplayText>
     </div>
 
-    <div className="flex col h5 mxa w-33-l w-33-m w-100">
+    <div className='flex col h5 mxa w-33-l w-33-m w-100'>
       <div>
-        {cv && (
-          <Button size="slim">
+        {cv &&
+          <Button size='slim'>
             <a href={cv} download>
               Download Cover Letter
             </a>
-          </Button>
-        )}
+          </Button>}
       </div>
       <div>
-        {application && (
-          <Button size="slim">
+        {application &&
+          <Button size='slim'>
             <a href={application} download>
               Download Application
             </a>
-          </Button>
-        )}
+          </Button>}
       </div>
       <div>
-        {resume && (
-          <Button size="slim">
+        {resume &&
+          <Button size='slim'>
             <a href={resume} download>
               Download Resume
             </a>
-          </Button>
-        )}
+          </Button>}
       </div>
     </div>
   </li>
-);
+)
